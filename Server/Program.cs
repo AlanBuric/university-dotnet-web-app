3168889aac0ff5427d13ffda5cb8665ccf859090
Alan Burić <134159250+AlanBuric@users.noreply.github.com>
2025-02-03 23:09:21 +0100

    fix: use CORS policy on the Web API

--- Server/Program.cs
+++ Server/Program.cs
@@ -3,26 +3,38 @@ using UniversityWebApp.Database;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllers().AddNewtonsoftJson();
+var frontendUrl = builder.Configuration["FrontendUrl"] ?? throw new ArgumentNullException("Environment variable 'FrontendUrl' not found.");
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new ArgumentNullException("Connection string 'DefaultConnection' not found.");
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+builder.Services.AddControllers().AddNewtonsoftJson();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowBlazorWASM", policy =>
+    {
+        policy.WithOrigins(frontendUrl)
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(connectionString));
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    db.Database.EnsureCreated(); 
+    db.Database.EnsureCreated();
 }
+
+app.UseCors("AllowBlazorWASM");
 
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
+    app.UseSwaggerUI(options => options.SwaggerEndpoint("/openapi/v1.json", "University API"));
 }
 
 app.UseHttpsRedirection();
